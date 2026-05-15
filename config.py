647a5d8e625b235424c2250a8d6bf79fa8740f10
STAR⭐WALKER <seitalimuratov@gmail.com>
2026-05-15 12:32:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
 import os
 
-TOKEN      = os.getenv("BOT_TOKEN",      "8298687789:AAFlgj6AF4LgjBK68qSpC5paiN9Oh3nSc6Q")
-MANAGER_ID = os.getenv("MANAGER_CHAT_ID", "2145487498")
-
-RECEPTION_LINK   = "https://t.me/careercenter_reception"
-ADMIN_LINK       = "https://t.me/career_center_3128"
-REQUIRED_CHANNEL = "@careercenter_fergana"
