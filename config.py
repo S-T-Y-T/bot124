d65a4744d5de654d44d1a5a1c7a1cfd725421f77
STAR⭐WALKER <seitalimuratov@gmail.com>
2026-05-15 12:28:29 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 TOKEN      = os.getenv("BOT_TOKEN",      "8298687789:AAFlgj6AF4LgjBK68qSpC5paiN9Oh3nSc6Q")
-MANAGER_ID = os.getenv("MANAGER_CHAT_ID", "8236824332")
+MANAGER_ID = os.getenv("MANAGER_CHAT_ID", "2145487498")
 
 RECEPTION_LINK   = "https://t.me/careercenter_reception"
-ADMIN_LINK       = "https://t.me/muhammadyusuf_4347"
+ADMIN_LINK       = "https://t.me/career_center_3128"
 REQUIRED_CHANNEL = "@careercenter_fergana"
